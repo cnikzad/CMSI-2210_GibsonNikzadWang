78b825e015ab2af675c11b849a711a55ce28bab7
cnikzad <cnikzad@lion.lmu.edu>
2026-09-08 18:16:33 -0700

    Homework 1 complete

---
+++ sayhello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char * argv[] ) {
+
+   char name[100];
+
+   printf( "What is your name? " );
+   fgets( name, sizeof(name), stdin );
+
+   printf( "Hello, %s!\n", name );
+
+   return 0;
+}
